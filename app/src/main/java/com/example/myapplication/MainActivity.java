894913d18fd256cf21951770a9cf1c1bc0dea732
Arsenko <minsenka@gmail.com>
2020-03-08 15:54:37 +0300

    now add is commited

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -15,6 +14,9 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    final String HEADING = "heading";
+    final String BODY = "body";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +25,24 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
-    public void init(){
-        ListView lv=findViewById(R.id.myListView);
-        lv.setAdapter(createAdapter());
+    public void init() {
+        ListView listView = findViewById(R.id.myListView);
+        listView.setAdapter(createAdapter());
     }
 
     @NonNull
     private BaseAdapter createAdapter() {
-        return new SimpleAdapter(this,prepareContent(),R.layout.list_adapter_item,new String[]{"heading","body"},new int[]{R.id.heading,R.id.body});
+        return new SimpleAdapter(this, prepareContent(), R.layout.list_adapter_item, new String[]{"heading", "body"}, new int[]{R.id.heading, R.id.body});
     }
 
     @NonNull
     private List<Map<String, String>> prepareContent() {
-        List<Map<String, String>> temp= new ArrayList<>();
+        List<Map<String, String>> temp = new ArrayList<>();
         String[] values = getString(R.string.large_text).split("\n\n");
-        for(int i=0;i<values.length;i++){
-            Map mapItem=new HashMap<String,String >();
-            mapItem.put("heading",String.valueOf(values[i].length()));
-            mapItem.put("body",values[i]);
+        for (int i = 0; i < values.length; i++) {
+            Map<String, String> mapItem = new HashMap<>();
+            mapItem.put(HEADING, String.valueOf(values[i].length()));
+            mapItem.put(BODY, values[i]);
             temp.add(mapItem);
         }
         return temp;
